ccab7d1f4e3ddf366a89271070791a76a23dd29e
sunjianfei1029 <sunjianfei1029@126.com>
2019-03-22 10:49:35 +0800

    版本3:添加登录验证

--- dailyfresh/df_user/urls.py
+++ dailyfresh/df_user/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     url(r'^register$',views.register),
     url(r'^register_handle$',views.register_handle),
     url(r'^login$',views.login),
+    url(r'^login_handle$',views.login_handle),
 ]
--- dailyfresh/df_user/views.py
+++ dailyfresh/df_user/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render,redirect
 from models import *
 from hashlib import sha1
+from django.contrib.auth import authenticate
 
 # 跳转到注册页面
 def register(request):
@@ -39,3 +40,38 @@ def register_handle(request):
 # 加载登录页面
 def login(request):
     return render(request,'df_user/login.html')
+
+# 登录验证
+def login_handle(request):
+    # 接受用户录入的用户名和密码
+    post=request.POST
+    uname = post.get('username')
+    upwd = post.get('pwd')
+
+    # 校验数据
+    if not all([uname,upwd]):
+        context = {'errmsg':'数据不完整'}
+        return render(request,'df_user/login.html',context)
+
+    # 使用django框架自带的认证校验用户名和密码
+    # user = authenticate(username=uname,password=upwd)
+    # 使用传统方式验证用户经和密码
+    s1 = sha1()
+    s1.update(upwd)
+    upwd2 = s1.hexdigest()
+    try:
+        user = UserInfo.objects.get(uname=uname, upwd=upwd2)
+    except :
+        user = None
+
+
+    if user is not None:
+       return render(request, 'df_index/index.html')
+
+    else:
+        # 报错
+        context = {'errmsg': '用户名密码不正确'}
+        return render(request, 'df_user/login.html', context)
+
+
+    return render(request,'df_user/login.html',{'errmsg':'用户名或密码不正确'})
